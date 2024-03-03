b17572d02abf807bbbc6c3068331d3b0939cb45f
Perrine Pullicino <perrinepullicino@protonmail.com>
2024-03-03 15:48:53 +0100

    Fix casing LastName and FirstName

--- backend/CanneTVReplay/Repositories/Fighter.cs
+++ backend/CanneTVReplay/Repositories/Fighter.cs
@@ -2,7 +2,7 @@
 {
     public class Fighter
     {
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
